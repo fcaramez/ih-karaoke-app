6a2be06c00614ea296c5f73163e0f3e06bb86480
fcaramez <francisco.caramez.duarte@gmail.com>
2022-10-13 15:26:39 +0100

    [KA-3] adds initial chakraUI config

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const chakraTheme = extendTheme({ config });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={chakraTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
