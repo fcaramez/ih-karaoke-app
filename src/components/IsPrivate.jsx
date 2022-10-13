3a889931b1aad1a4470d250eab9d4bbe510a0d6d
fcaramez <francisco.caramez.duarte@gmail.com>
2022-10-13 18:51:36 +0100

    adds a spinner while loading

--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -1,10 +1,29 @@
 import { AuthProvider, useAuth } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
+import DotLoader from "react-spinners/DotLoader";
 
 function IsPrivate({ children }) {
   const { isLoggedIn, isLoading } = useAuth(AuthProvider);
 
-  if (isLoading) return <p>Loading ...</p>;
+  const override = {
+    display: "block",
+    marginLeft: "50vw",
+    marginRight: "50vw",
+    marginTop: "50vh",
+    marginBottom: "50vh",
+    borderColor: "aqua",
+  };
+
+  if (isLoading)
+    return (
+      <DotLoader
+        color={"aqua"}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    );
 
   if (!isLoggedIn) {
     return <Navigate to={`/login`} />;
