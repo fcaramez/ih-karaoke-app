5f8ab1e13b121c028c1bf2468f34435e6b1cf7e1
fcaramez <francisco.caramez.duarte@gmail.com>
2022-10-13 17:31:55 +0100

    adds login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,96 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  useColorModeValue,
+  Center,
+} from "@chakra-ui/react";
+import GoogleButton from "react-google-button";
+import ihLogo from "../assets/ironhack-logo.png";
+import { Image } from "@chakra-ui/react";
+import {
+  logInWithEmailAndPassword,
+  signInWithGoogle,
+} from "../firebaseFuntions/auth.firebase";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Center>
+          <Stack align={"center"}>
+            <Center>
+              <Image
+                src={ihLogo}
+                style={{
+                  maxWidth: "25%",
+                  maxHeight: "25%",
+                  display: "flex",
+                  alignContent: "center",
+                  justifyContent: "center",
+                }}
+                alt="ironhack logo"></Image>
+            </Center>
+            <Heading fontSize={"4xl"}>Login for Karaoke!🎤</Heading>
+          </Stack>
+        </Center>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" onChange={(e) => setEmail(e.target.value)} />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}>
+                <Center>
+                  <Link color={"blue.400"}>Forgot password?</Link>
+                </Center>
+              </Stack>
+              <Button
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+                onClick={() => {
+                  logInWithEmailAndPassword(email, password);
+                }}>
+                Sign up
+              </Button>
+              <GoogleButton
+                onClick={() => signInWithGoogle()}
+                style={{ width: "100%" }}></GoogleButton>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
