617f6e38732835eea7b30e3d7378912be0744084
fcaramez <francisco.caramez.duarte@gmail.com>
2022-10-13 15:39:30 +0100

    [KA-4] Styled-Components initial setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import GlobalStyle from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  color: {},
+};
 
 const config = {
   initialColorMode: "dark",
@@ -15,7 +21,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={chakraTheme}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
