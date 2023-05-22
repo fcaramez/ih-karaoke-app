b52fb6ff9fa53e4b0cb20cbb05359c373d333e4a
fcaramez <francisco.caramez.duarte@gmail.com>
2023-05-22 17:12:57 +0100

    changes UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,21 +22,25 @@ import {
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
- 
 
   return (
     <Flex
       minH={"100vh"}
       align={"center"}
       justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}>
-      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack
+        spacing={8}
+        mx={"auto"}
+        maxW={"lg"}
+        py={12}
+        px={6}
+      >
         <Center>
           <Stack align={"center"}>
             <Center>
@@ -49,7 +53,8 @@ export default function Login() {
                   alignContent: "center",
                   justifyContent: "center",
                 }}
-                alt="ironhack logo"></Image>
+                alt="ironhack logo"
+              ></Image>
             </Center>
             <Heading fontSize={"3xl"}>Login for Karaoke!🎤</Heading>
           </Stack>
@@ -58,28 +63,44 @@ export default function Login() {
           rounded={"lg"}
           bg={useColorModeValue("white", "gray.700")}
           boxShadow={"lg"}
-          p={8}>
+          p={8}
+        >
           <Stack spacing={4}>
+            <Button
+              onClick={() => signInWithGoogle()}
+              w={"full"}
+              variant={"outline"}
+              leftIcon={<FcGoogle />}
+            >
+              <Center>
+                <Text>Sign in with Google</Text>
+              </Center>
+            </Button>
             <FormControl id="email">
               <FormLabel>Email address</FormLabel>
-              <Input type="email" onChange={(e) => setEmail(e.target.value)} />
+              <Input
+                type="email"
+                onChange={e => setEmail(e.target.value)}
+              />
             </FormControl>
             <FormControl id="password">
               <FormLabel>Password</FormLabel>
               <Input
                 type="password"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={e => setPassword(e.target.value)}
               />
             </FormControl>
             <Stack spacing={10}>
               <Stack
                 direction={{ base: "column", sm: "row" }}
                 align={"start"}
-                justify={"space-between"}>
+                justify={"space-between"}
+              >
                 <Stack>
                   <Link
                     color={"blue.400"}
-                    onClick={() => navigate("/reset-password")}>
+                    onClick={() => navigate("/reset-password")}
+                  >
                     Forgot password?
                   </Link>
                 </Stack>
@@ -92,17 +113,9 @@ export default function Login() {
                 }}
                 onClick={() => {
                   logInWithEmailAndPassword(email, password);
-                }}>
+                }}
+              >
                 Login
-              </Button>
-              <Button
-                onClick={() => signInWithGoogle()}
-                w={"full"}
-                variant={"outline"}
-                leftIcon={<FcGoogle />}>
-                <Center>
-                  <Text>Sign in with Google</Text>
-                </Center>
               </Button>
             </Stack>
           </Stack>
